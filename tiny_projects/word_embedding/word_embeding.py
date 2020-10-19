83c8bd4823904cedc5b49af59af8bc52a5c9757e
mingzhu <15216802238@163.com>
2020-10-19 16:00:11 +0800

    2020.10.19

--- tiny_projects/word_embedding/word_embeding.py
+++ tiny_projects/word_embedding/word_embeding.py
@@ -1,13 +1,17 @@
-#这里使用的预计算的词嵌入数据库是GloVe(global vectors for word representation)
+#根据矩阵乘法，从输入层到嵌入层相当于简单的查找，更形象的说相当于字典查找
 from keras.datasets import imdb
 from keras import preprocessing
 from keras import models
 from keras.layers import Flatten,Embedding,Dense
-
+#句子中只包含全部语料最常出现的10000的词，在此之外的忽略掉
 max_features = 10000
+#句子长度限定为20，即保留句子中20个最常出现的词，不足部分用零补充
 maxlen = 20
 (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
+print(x_train[0])
+#embedding层的激活形状为(samples,maxlen,8)
 x_train = preprocessing.sequence.pad_sequences(x_train, maxlen=maxlen)
+print(x_train[0])
 x_test = preprocessing.sequence.pad_sequences(x_test, maxlen=maxlen)
 model = models.Sequential()
 model.add(Embedding(10000, 8, input_length=maxlen))
--- utility/one_hot_code.py
+++ utility/one_hot_code.py
@@ -27,6 +27,35 @@ print(results)
 
 
 #字符级one-hot编码
+#所有可打印的ASCII字符
+characters  =string.printable
+token_index2 = dict(zip(characters,range(0,len(characters))))
+print(token_index2)
+max_length2 = 30
+results2 = np.zeros((len(samples),max_length2,max(token_index2.values())))
+
+for i,sample in enumerate(samples):
+    for j,character in list(enumerate(sample))[:max_length2]:
+        index = token_index2.get(character)
+        results2[i,j,index] = 1
+
+
+print(results2[0,1])
+#--------------------------------------------------------------------------------
+#使用Keras实现one-hot
+from keras.preprocessing.text import Tokenizer
+samples = ['The cat sat on the mat.', 'The dog ate my homework.']
+#只考虑20个最常见的词。
+tokenizer = Tokenizer(num_words=5)
+#用samples构建单词索引
+tokenizer.fit_on_texts(samples)
+sequence = tokenizer.texts_to_sequences(samples)
+print(sequence)
+word_index = tokenizer.word_index
+print(word_index)
+#可以直接转化为ont-hot形式。不过是句子形式的
+one_hot = tokenizer.texts_to_matrix(samples,mode='binary')
+print(one_hot)
 
 
 
