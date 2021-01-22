4fd22b1aeeb5367a5132310c211fc80c514eb3e4
mingzhu <15216802238@163.com>
2021-01-22 14:29:09 +0800

    32

--- trail.py
+++ trail.py
@@ -1,5 +1,17 @@
-import numpy as np
+from keras.datasets import mnist
+from keras import models
+from keras import layers
+from keras.utils import to_categorical
+from utility.graphic import training_and_validation_loss
 
-a = np.array([1,2,3,4])
-print(a[:-1])
-print(a[:-2])
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+print(train_images[0].shape)
+# print(train_images[0])
+from PIL import Image
+
+img1 = Image.fromarray(train_images[0])
+img1 = img1.resize((14,28))
+img1.save("./minist1.jpg")
+import numpy as np
+array1 = np.array(img1)
+# print(array1)
